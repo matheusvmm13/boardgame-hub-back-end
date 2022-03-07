00a8e3966b214657d8ab6a7d576e2bb6da536a2c
Matheus Martins <matheusvmm13@gmail.com>
2022-03-07 15:31:27 +0100

    Added errors middleware

--- src/server/index.js
+++ src/server/index.js
@@ -1,9 +1,11 @@
 require("dotenv").config();
 const express = require("express");
+const cors = require("cors");
 const { default: helmet } = require("helmet");
 const morgan = require("morgan");
 
 const app = express();
+app.use(cors());
 app.use(morgan("dev"));
 app.use(express.json());
 app.use(helmet());
