066edd056888ba63dd52170517aefc708faaadac
Matheus Martins <matheusvmm13@gmail.com>
2022-03-07 16:42:43 +0100

    Connected database successefully

--- src/index.js
+++ src/index.js
@@ -3,11 +3,14 @@ const debug = require("debug")("boardgame:root");
 const startServer = require("./server/startServer");
 
 const app = require("./server/index");
+const connectDataBase = require("./database");
 
 const port = process.env.PORT || 4000;
+const connectionString = process.env.MONGO_STRING;
 
 (async () => {
   try {
+    await connectDataBase(connectionString);
     await startServer(app, port);
   } catch {
     debug("The server is broken");
--- src/server/index.js
+++ src/server/index.js
@@ -3,11 +3,15 @@ const express = require("express");
 const cors = require("cors");
 const { default: helmet } = require("helmet");
 const morgan = require("morgan");
+const { notFoundError, generalError } = require("./middlewares/errors");
 
 const app = express();
 app.use(cors());
 app.use(morgan("dev"));
 app.use(express.json());
 app.use(helmet());
+
+app.use(notFoundError);
+app.use(generalError);
 
 module.exports = app;
