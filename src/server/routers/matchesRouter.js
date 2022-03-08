c4c0c52fd31ce133b8d995f05d4d18b957a946d5
Matheus Martins <matheusvmm13@gmail.com>
2022-03-08 21:03:11 +0100

    Fixed routes path

--- src/server/routers/matchesRouter.js
+++ src/server/routers/matchesRouter.js
@@ -2,6 +2,6 @@ require("dotenv").config();
 const matchesRouter = require("express").Router();
 const getAllMatches = require("../controllers/matchController/matchController");
 
-matchesRouter.get("/matches", getAllMatches);
+matchesRouter.get("/matches/", getAllMatches);
 
 module.exports = matchesRouter;
