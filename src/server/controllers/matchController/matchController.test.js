b6903622b8977090db4dd87c8412cea4053f24e9
Matheus Martins <matheusvmm13@gmail.com>
2022-03-09 17:24:39 +0100

    Created supertest for delete case

--- src/server/controllers/matchController/matchController.test.js
+++ src/server/controllers/matchController/matchController.test.js
@@ -1,9 +1,11 @@
+/* eslint-disable no-underscore-dangle */
 require("dotenv").config();
 const { MongoMemoryServer } = require("mongodb-memory-server");
 const { default: mongoose } = require("mongoose");
 const request = require("supertest");
 const app = require("../..");
 const connectDataBase = require("../../../database");
+const Match = require("../../../database/models/Match");
 
 let mongoServer;
 
@@ -11,11 +13,17 @@ beforeAll(async () => {
   mongoServer = await MongoMemoryServer.create();
   const connectionMemoryString = mongoServer.getUri();
   await connectDataBase(connectionMemoryString);
-});
 
-afterAll(async () => {
-  await mongoose.connection.close();
-  await mongoServer.stop();
+  await Match.create({
+    gameTitle: "Carcasone",
+    id: "6217ceafa37925757491ca2c",
+    image: "",
+    creator: "",
+    date: "2022-03-27T16:30:00.000+00:00",
+    players: ["6227adb37ab103d1d964c91f", "6227adb37ab103d1d964c91f"],
+    maxPlayers: 6,
+    location: "Madrid",
+  });
 });
 
 describe("Given a /matches endpoint", () => {
@@ -24,4 +32,22 @@ describe("Given a /matches endpoint", () => {
       await request(app).get("/matches/").expect(200);
     });
   });
+});
+
+describe("Given a /my-matches/id endpoint", () => {
+  describe("When it receives a DELETE request", () => {
+    test("Then it should reply with a 200 status code", async () => {
+      const matchToBeDeleted = await Match.findOne();
+
+      await request(app)
+        .delete(`/my-matches/${matchToBeDeleted._id}`)
+        .expect(200);
+    });
+  });
+});
+
+afterAll(async () => {
+  await mongoose.connection.close();
+  await mongoServer.stop();
+  await Match.deleteMany({});
 });
