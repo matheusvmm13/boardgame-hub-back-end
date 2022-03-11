43ccc4449bfc2828dadd6937f281792f958bcf3e
Matheus Martins <matheusvmm13@gmail.com>
2022-03-11 11:22:54 +0100

    Created the login function

--- src/server/routers/usersRouter.js
+++ src/server/routers/usersRouter.js
@@ -1,7 +1,9 @@
 require("dotenv").config();
 const usersRouter = require("express").Router();
+const loginController = require("../controllers/usersController/loginController");
 const getUsers = require("../controllers/usersController/userController");
 
+usersRouter.post("/users/login/", loginController);
 usersRouter.get("/users/", getUsers);
 
 module.exports = usersRouter;
