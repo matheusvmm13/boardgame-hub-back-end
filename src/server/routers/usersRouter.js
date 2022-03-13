12e3d73adf7c15555695ec6dc7b73973aa9ab0a4
Matheus Martins <matheusvmm13@gmail.com>
2022-03-13 23:31:36 +0100

    Added endpoint to get user by id

--- src/server/routers/usersRouter.js
+++ src/server/routers/usersRouter.js
@@ -1,9 +1,13 @@
 require("dotenv").config();
 const usersRouter = require("express").Router();
 const loginController = require("../controllers/usersController/loginController");
-const getUsers = require("../controllers/usersController/userController");
+const {
+  getUsers,
+  getUserById,
+} = require("../controllers/usersController/userController");
 
 usersRouter.get("/users/", getUsers);
+usersRouter.get("/users/:id", getUserById);
 usersRouter.post("/users/login/", loginController);
 
 module.exports = usersRouter;
