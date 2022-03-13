30af8d7f56f5e64b8a45d015c353421afa4c1274
Matheus Martins <matheusvmm13@gmail.com>
2022-03-13 18:19:51 +0100

    Refactor matches routes

--- src/server/routers/usersRouter.js
+++ src/server/routers/usersRouter.js
@@ -1,13 +1,9 @@
 require("dotenv").config();
 const usersRouter = require("express").Router();
-const {
-  getMyMatches,
-} = require("../controllers/matchController/matchController");
 const loginController = require("../controllers/usersController/loginController");
 const getUsers = require("../controllers/usersController/userController");
 
 usersRouter.get("/users/", getUsers);
-usersRouter.get("/users/my-matches/:id", getMyMatches);
 usersRouter.post("/users/login/", loginController);
 
 module.exports = usersRouter;
