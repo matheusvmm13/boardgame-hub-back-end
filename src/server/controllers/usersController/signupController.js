e611cb051bf2aa779bc7fde3b55a15bc1228c775
Matheus Martins <matheusvmm13@gmail.com>
2022-03-18 18:48:01 +0100

    Created the signup controller

---
+++ src/server/controllers/usersController/signupController.js
@@ -0,0 +1,32 @@
+const bcrypt = require("bcrypt");
+const User = require("../../../database/models/User");
+
+const signupController = async (req, res, next) => {
+  const { name, username, password } = req.body;
+
+  if (!name || !username || !password) {
+    const error = new Error("You must provide a name, username and password");
+    error.status = 400;
+    next(error);
+  } else {
+    const repitedUser = await User.findOne({ username });
+
+    if (repitedUser) {
+      const error = new Error("This username isn't avaliable");
+      error.status = 400;
+      next(error);
+    } else {
+      const hashedPassword = await bcrypt.hash(password, 10);
+      const newUser = {
+        name,
+        username,
+        password: hashedPassword,
+      };
+
+      const createdUser = await User.create(newUser);
+      res.json(createdUser);
+    }
+  }
+};
+
+module.exports = signupController;
--- src/server/routers/usersRouter.js
+++ src/server/routers/usersRouter.js
@@ -1,9 +1,11 @@
 require("dotenv").config();
 const usersRouter = require("express").Router();
 const loginController = require("../controllers/usersController/loginController");
+const signupController = require("../controllers/usersController/signupController");
 const getUsers = require("../controllers/usersController/userController");
 
 usersRouter.get("/users/", getUsers);
 usersRouter.post("/users/login/", loginController);
+usersRouter.post("/users/signup/", signupController);
 
 module.exports = usersRouter;
